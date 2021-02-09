b64aa8983dd83d3bf77c822a5ae59a630b221c1e
elvisAR-git <alanray459@gmail.com>
2021-02-09 16:27:15 +0300

    added nav dashboard

--- src/logic/mainControler.js
+++ src/logic/mainControler.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const ARender = require("../main").ARender;
-
+const mediaRender = require("../mediaRender");
 const Router = express.Router();
 const loadApps = require("./loadApps");
 
-Router.get("", (req, res) => {
+Router.get("/web", (req, res) => {
   var apps = loadApps();
 
   var page = ARender.renderPage(apps);
   res.send(page);
 });
 
+Router.get("", (req, res) => {
+  file = mediaRender.get("home.html");
+  file.then((data) => {
+    if (data.code == 200) {
+      // file found
+      res.status(data.code);
+      res.write(data.data);
+      res.end();
+    } else {
+      res.status(data.code);
+      res.write(req.params.filename + " could not be found on this server");
+      res.end();
+    }
+  });
+});
 module.exports = Router;
